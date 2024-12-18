db562463db74299d0f63ce60d15969724c5164f7
KlowJK <thanghavan2004@gmail.com>
2024-12-18 15:00:06 +0700

    second commit

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class BookController extends Controller
 {
@@ -12,6 +13,8 @@ class BookController extends Controller
     public function index()
     {
         //
+        $books = Book::all();
+        return view('books.index', compact('books'));
     }
 
     /**
